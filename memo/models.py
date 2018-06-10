56284aac4e75e940211871d58c2ec838c189a834
Jungsik Choi <chjs@skku.edu>
2018-06-10 09:08:58 +0900

    memo 추가하는 form 생성 및 bootstrap 설정

--- memo/models.py
+++ memo/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 
 class Memo(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField(max_length=200, verbose_name="제목")
+    text = models.TextField(verbose_name="내용")
     created_date = models.DateTimeField(
             default=timezone.now)
 
--- memo/views.py
+++ memo/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+# pylint: disable=E1101
+from django.shortcuts import render, redirect
 from .models import Memo
+from .forms import MemoForm
 
 # Create your views here.
 def memo_list(request):
     qs = Memo.objects.all()
-    return render(request, 'memo/memo_list.html', {'memos': qs})
+    return render(request, 'memo/memo_list.html', {'memos' : qs})
 
+def memo_new(request):
+    if request.method == "POST":
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            memo = Memo()
+            memo.title = form.cleaned_data['title']
+            memo.text = form.cleaned_data['text']
+            memo.author = request.user
+            memo.save()
+            return redirect('memo_list')
+    else:
+        form = MemoForm()
+    return render(request, 'memo/memo_edit.html', {'form' : form})
