3f1201e5bf121779dc7dcbb2ac636756a4053ada
Jungsik Choi <chjs@skku.edu>
2018-06-17 20:17:45 +0900

    json 응답 추가

--- memo/models.py
+++ memo/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 
 
 class Memo(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
--- memo/views.py
+++ memo/views.py
@@ -1,11 +1,13 @@
 # pylint: disable=E1101
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Memo
 from .forms import MemoForm
+import json
 
 # Create your views here.
 def memo_list(request):
-    qs = Memo.objects.all()
+    qs = Memo.objects.all().order_by("-created_date")
 
     q = request.GET.get('q', '')
     if q:
@@ -15,8 +17,29 @@ def memo_list(request):
             message = "검색 결과가 없습니다."
             return render(request, 'memo/memo_list.html', {'msg' : message})
         qs = result_qs
+    else:
+        if not qs:
+            message = "메모가 하나도 없다."
+            return render(request, 'memo/memo_list.html', {'msg' : message})
 
     return render(request, 'memo/memo_list.html', {'memos' : qs})
+
+
+def memo_list_json(request):
+    memos_dic = {}
+    records_list = []
+    qs = Memo.objects.all().order_by('-created_date')
+
+    for memo in qs:
+        title = memo.title
+        text = memo.text
+        record = {"title":title, "text":text}
+        records_list.append(record)
+    
+    memos_dic["memos"] = records_list
+    return HttpResponse(json.dumps(memos_dic, ensure_ascii=False), content_type="application/json")
+
+
 
 def memo_new(request):
     if request.method == "POST":
